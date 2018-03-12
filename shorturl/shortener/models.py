46a9be20ac7ec3b49be932c9c4ca6e05075642e3
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-03-11 19:19:01 -0500

    added class method on Url model to return the encoded url

--- shorturl/shortener/models.py
+++ shorturl/shortener/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from . import base34
 
 class Url(models.Model):
 
     url = models.URLField(max_length=1000)
+
+    def encoded_url(self):
+        return '/' + base34.encode(self.pk)
